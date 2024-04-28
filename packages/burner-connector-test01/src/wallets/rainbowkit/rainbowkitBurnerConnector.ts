39f8b8bc1c4da071fb374a962250b7fe9b0c414b
Shiv Bhonde <shivbhonde04@gmail.com>
2024-04-28 22:19:53 +0530

    fix lint formatting

--- packages/burner-connector-test01/src/wallets/rainbowkit/rainbowkitBurnerConnector.ts
+++ packages/burner-connector-test01/src/wallets/rainbowkit/rainbowkitBurnerConnector.ts
@@ -1,6 +1,6 @@
 import type { WalletDetailsParams } from "@rainbow-me/rainbowkit";
-import type { EIP1193RequestFn, Transport, WalletRpcSchema } from "viem";
 import { createConnector } from "@wagmi/core";
+import type { EIP1193RequestFn, Transport, WalletRpcSchema } from "viem";
 import { burner } from "../../burnerConnector/burner.js";
 
 type Provider = ReturnType<
